77af943024114b15dcd28ea048989f2404b1f092
tien.blt <buitien747@gmail.com>
2025-01-20 15:59:20 +0700

    add username field for Account entity

--- MobileApp/src/main/java/com/example/MobileApp/Entity/Account.java
+++ MobileApp/src/main/java/com/example/MobileApp/Entity/Account.java
@@ -16,6 +16,8 @@ public class Account implements Serializable{
 	private static final long serialVersionUID = -1045283451506266552L;
 	@Id
 	private int accountId;
+	// thêm username
+	private String username;
 	private String email;
 	private String password;
 	
